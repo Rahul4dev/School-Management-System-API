6d0ec49537b9e417432c2c8966322dfb55858fef
Rahul4dev <rahulsinghsignin@gmail.com>
2023-11-09 17:32:53 +0530

    Express Routes Chaining

--- routes/academic/academicYear.js
+++ routes/academic/academicYear.js
@@ -16,10 +16,22 @@ const isAdmin = require('../../middlewares/isAdmin.js');
 const isLoggedIn = require('../../middlewares/isLoggedIn.js');
 
 // Routes
-academicYearRouter.post('/', isLoggedIn, isAdmin, createAcademicYear);
-academicYearRouter.get('/', isLoggedIn, isAdmin, getAllAcademicYears);
-academicYearRouter.get('/:id', isLoggedIn, isAdmin, getSingleAcademicYear);
-academicYearRouter.put('/:id', isLoggedIn, isAdmin, updateAcademicYear);
-academicYearRouter.delete('/:id', isLoggedIn, isAdmin, deleteAcademicYear);
+// academicYearRouter.post('/', isLoggedIn, isAdmin, createAcademicYear);
+// academicYearRouter.get('/', isLoggedIn, isAdmin, getAllAcademicYears);
+
+academicYearRouter
+  .route('/')
+  .post(isLoggedIn, isAdmin, createAcademicYear)
+  .get(isLoggedIn, isAdmin, getAllAcademicYears);
+
+// academicYearRouter.get('/:id', isLoggedIn, isAdmin, getSingleAcademicYear);
+// academicYearRouter.put('/:id', isLoggedIn, isAdmin, updateAcademicYear);
+// academicYearRouter.delete('/:id', isLoggedIn, isAdmin, deleteAcademicYear);
+
+academicYearRouter
+  .route('/:id')
+  .get(isLoggedIn, isAdmin, getSingleAcademicYear)
+  .put(isLoggedIn, isAdmin, updateAcademicYear)
+  .delete(isLoggedIn, isAdmin, deleteAcademicYear);
 
 module.exports = academicYearRouter;
